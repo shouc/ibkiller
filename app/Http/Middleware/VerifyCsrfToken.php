67accd4a3a802c82cd978cf2098be6f3c035f884
kotobukki <scf@ieee.org>
2018-08-19 09:49:18 +0800

    2

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,6 +15,6 @@ class VerifyCsrfToken extends Middleware
         'http://127.0.0.1:8000/*',
         'http://localhost:8000/*',
         'http://admin.dev.zwang.internal:8000/*',
-        'http://admin.ibkiller.com/*',
+        'http://admin.ibkiller.com:8000/*',
     ];
 }
