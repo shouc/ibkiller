f88d7629303a03210cb5bc6a0b90bcf5b8966ac3
Shou C <scf@ieee.org>
2018-12-15 15:01:32 +0800

    finish partly #4

--- app/Http/Controllers/MatesController.php
+++ app/Http/Controllers/MatesController.php
@@ -26,13 +26,13 @@ class MatesController extends Controller
         $radLng2 = deg2rad($to[1]);
         $a = $radLat1 - $radLat2;
         $b = $radLng1 - $radLng2;
-        $distance = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2))) * 6378.137 * 1000;
+        $distance = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2))) * 6378.137;
         return $distance;
     }
 
     public function calculateDeterminant($mateUndone, $_grade, $parsedLocation)
     {
-        $gradeDeterminant = (float)abs($mateUndone->grade - $_grade) * 0.6 / 12;
+        $gradeDeterminant = (float)abs($mateUndone->grade - $_grade) * 0.6 / 4;
         $parsedMateLocation = explode('!!', $mateUndone->location);
         $distanceDeterminant = $this->calculateDistance($parsedLocation, $parsedMateLocation) * 0.4/ 14000;
         unset($parsedMateLocation);
@@ -54,12 +54,12 @@ class MatesController extends Controller
     }
     public function findMate(Request $request)
     {   
-        if ($request->has(['Grade', 'Location', 'Preference', 'Gender'])) {
+        if ($request->has(['Grade', 'Location', 'Interest', 'Gender'])) {
             $_session = Cookie::get('ibkiller_session');
             $_grade = (int)($request->Grade);
             $_location = $request->Location;
-            $parsedLocation = explode('!!', $_location);
-            $_preference = $request->Preference;
+            $parsedLocation = explode('!!', $_location); //lat!!lng
+            $_preference = $request->Interest;
             $_gender = (int)($request->Gender); //0 for male, 1 for female
             if ($_session){
                 $isLoggedIn = true;
@@ -67,23 +67,32 @@ class MatesController extends Controller
                 $isLoggedIn = false;
                 return redirect('/');
             }
+            if(DB::table('mate_info')
+                ->where('session', $_session)
+                ->get() != "[]"){
+                return 'already';
+            }
             DB::table('mate_info')
                 ->insert([
                     'session' => $_session,
                     'done' => 0,
                     'grade' => $_grade,
+                    'gender' => $_gender,
                     'location' => $_location,
                     'preference' => $_preference,
                     'time' => time()
                 ]);
             $allMatesUndone = DB::table('mate_info')
-                ->where([['done', 0], ['gender', '!=', $_gender]])
+                ->where([
+                    ['done', 0], 
+                    ['gender', '!=', $_gender],
+                ])
                 ->get()
                 ->toArray();
-            $highestDeterminant = 0.9;
+            $highestDeterminant = 0.8;
             $bestMatch = null;
             foreach ($allMatesUndone as $i => $mateUndone) {
-                $currentDeterminant = calculateDeterminant($mateUndone, $_grade, $parsedLocation);
+                $currentDeterminant = $this->calculateDeterminant($mateUndone, $_grade, $parsedLocation);
                 if ($currentDeterminant >= $highestDeterminant){
                     $highestDeterminant = $currentDeterminant;
                     $bestMatch = $mateUndone;
@@ -92,7 +101,21 @@ class MatesController extends Controller
             if ($bestMatch == null){
                 return 'No mate found.';
             } else {
-                return $bestMatch;
+                DB::table('mate_info')
+                    ->where('session', $_session)
+                    ->update(['done' => 1]);
+                DB::table('mate_info')
+                    ->where('session', $_session)
+                    ->update(['assigned_session' => $bestMatch->session]);
+                DB::table('mate_info')
+                    ->where('session', $bestMatch->session)
+                    ->update(['done' => 1]);
+                DB::table('mate_info')
+                    ->where('session', $bestMatch->session)
+                    ->update(['assigned_session' => $_session]);
+                unset($bestMatch->session);
+                unset($bestMatch->assigned_session);
+                return json_encode($bestMatch);
             }
         } else {
             return redirect('/mate');
