cbb1fdda95fb67153cb1de004f9611cd7e4f5941
Shou C <scf@ieee.org>
2018-12-23 15:44:02 +0800

    noMate.blade.php rowback

--- resources/views/noMate.blade.php
+++ resources/views/noMate.blade.php
@@ -1,15 +1,49 @@
-1
-<button onclick="test()"></button>
-<script type="text/javascript">
-    function test(){
-        history.pushState(null, null, document.URL + '?back');
-        window.addEventListener('popstate', function () {
-            alert(1);
-        });
-    }
-    //window.onpopstate = function(event) {
-    //  alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
-    //};
+@include('header', ['css' => '.mateRegister.css'])
 
-    
+@include('nav', ['q' => false])
+<style type="text/css">
+body {
+  background-color: #fff;
+}
+.matingForm {
+  margin-top: 120px;
+  margin-bottom: 20px;
+}
+.matingStep {
+  color: #aaa;
+  font-size: 20px;
+}
+</style>
+<body>
+
+<!--
+<div class="is-pay">
+  <p class="is-pay-title">Paid User</p>
+  <p class="is-pay-desc">You could select your mate</p>
+  <p class="is-pay-price">$10<a class="is-pay-year"> / year</a></p>
+</div>
+-->
+
+<form class="container matingForm" id="form-1" action="/findMate" style="display: none;">
+  <h1 class="h1-local">
+	  <h class="matingStep">2/2</h> Get you a suitable mate
+  </h1>
+  <div class="alert alert-danger" role="alert">
+    Sorry! There has no study mate suitable for you. However, we have put your request into our database. We will notify you by Email whenever we find you a study mate : )
+  </div>
+
+</form>
+
+
+</body>
+
+<script type="text/javascript">
+  width = window.innerWidth;
+  height = $(window).height();
+  if (width > 800) {
+   $("#form-1").addClass("col-5")
+  } else {
+    $("#form-1").addClass("col-lg-2")
+  }
+  $("#form-1").show();
 </script>
